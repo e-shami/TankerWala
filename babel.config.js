b351291244c323ce46730bd539cc6e959c1ab9af
Ehtisham Rehmat <a7rehmat@gmail.com>
2023-05-22 10:05:53 +0500

    changing some settings

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // Other plugins...
+    [
+      'react-native-reanimated/plugin',
+      {
+        'relativeSourceLocation': true,
+      },
+    ],
+  ],
 };
